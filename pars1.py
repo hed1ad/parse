048372c3a6886dea9af528d23da96cb5a57e5479
hed1ad <84936556+hed1ad@users.noreply.github.com>
2022-08-04 16:06:42 +0300

    Update pars1.py

--- pars1.py
+++ pars1.py
@@ -21,25 +21,7 @@ def start(message):
 def vacancy(message):
     msg=bot.send_message(message.chat.id, "Введите название вакансии")
     bot.register_next_step_handler(msg, search)
-'''
-@bot.message_handler(commands=['gut'])
-def gut(message):
-    bot.send_message(message.chat.id, 'Загружаю последние новости')
-    naz='https://www.sut.ru/bonchevents'
-    driver.get(naz)
-    sleep(2)
-    try:
-        for i in range(1,6):
-            if i !=3:
-                gutn=driver.find_elements_by_class_name('vt287')
-                bot.send_message(message.chat.id, gutn[0].get_attribute('href'))
-                
-            else:
-                print('lol')
-    except:
-        bot.send_message(message.chat.id, 'Новостей нет') 
 
-'''
 @bot.message_handler(content_types=['text'])
 def text(message):
     bot.send_message(message.chat.id, 'Ты что то хотел?')
